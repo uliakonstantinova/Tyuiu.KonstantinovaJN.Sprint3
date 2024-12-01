af817a8367c47739899410dee6fec2bd479311f8
Юлия Константинова <Tyuiu.Konstantinovaajn@outlook.com>
2024-12-01 23:44:22 +0500

    Task7

---
+++ Tyuiu.KonstantinovaJN.Sprint3.Task7.V28.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.KonstantinovaJN.Sprint3.Task7.V28.Lib
+{
+    public class DataService : ISprint3Task7V28
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+
+            double y;
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                double u = Math.Cos(2 * x);
+                double l = u + (Math.Sin(x) / (x + 2.5));
+                y = Math.Round((l + 2 * x), 2);
+                valueArray[count] = y;
+                count++;
+            }
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.KonstantinovaJN.Sprint3.Task7.V28/Program.cs
@@ -0,0 +1,57 @@
+﻿using Tyuiu.KonstantinovaJN.Sprint3.Task7.V28.Lib;
+
+namespace Tyuiu.KonstantinovaJN.Sprint3.Task6.V15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт 3 | Выполнила: Константинова Ю. Н.| ИСПБ-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #28                                                             *");
+            Console.WriteLine("* Выполнила: Константинова Юлия Николаевна | ИСПБ-24-1                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая выводит таблицу значений функции на заданном*");
+            Console.WriteLine("* диапазоне [-5, 5] с шагом 1. Значения занести в массив!                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+
+            double[] valueArray;
+            valueArray = new double[len];
+
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("+----------+----------+");
+            Console.WriteLine("|    X     |   F(x)   |");
+            Console.WriteLine("+----------+----------+");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("|{0,5:d}     |  {1, 5:f2}   |", startValue, valueArray[i]);
+                startValue++;
+            }
+            Console.WriteLine("+----------+----------+");
+            Console.ReadKey();
+
+        }
+    }
+}
